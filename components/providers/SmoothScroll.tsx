a1c7c82d8ccea9e387fe459209a621b060746e5b
Piyush Mishra <piyush.mishra@mygenie.online>
2026-06-30 19:58:04 +0530

    lenis package related issue

--- components/providers/SmoothScroll.tsx
+++ components/providers/SmoothScroll.tsx
@@ -1,11 +1,35 @@
 "use client";
-import { ReactLenis } from "@studio-freight/react-lenis";
-import { ReactNode } from "react";
 
-export default function SmoothScroll({ children }: { children: ReactNode }) {
-  return (
-    <ReactLenis root options={{ lerp: 0.15, smoothWheel: false,  }}>
-      {children as any}
-    </ReactLenis>
-  );
+import { ReactNode, useEffect } from "react";
+import Lenis from "lenis";
+
+interface SmoothScrollProps {
+  children: ReactNode;
+}
+
+export default function SmoothScroll({
+  children,
+}: SmoothScrollProps) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.15,
+      smoothWheel: false,
+    });
+
+    let animationFrameId: number;
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      animationFrameId = requestAnimationFrame(raf);
+    };
+
+    animationFrameId = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
 }
